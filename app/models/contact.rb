52fd5e90bea69ceabd0efa221b5e3cb3956be1b3
bobomimi <boaz242@gmail.com>
2015-12-17 21:28:35 +0200

    f

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,8 @@
 class Contact < MailForm::Base
-	attribute :name,      :validate => true
-	attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-	attribute :message,   :validate => true
+	attribute :name,         :validate => true
+	attribute :email,        :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+	attribute :message,      :validate => true
+	attribute :mobile_phone, :validate => true
 
 	def headers 
 		{
